5cc4b3aa4cda73aa3f54c330fabf31bbc9d073c6
hhe8 <hhe8@uchicago.edu>
2015-07-14 19:42:09 -0500

    fix redirect and id

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -4,13 +4,13 @@ class PlacesController < ApplicationController
   end
 
   def show
-      @place = Place.find_by(:title => params["id"])
+      @place = Place.find_by(:id => params["id"])
   end
 
   def delete
-      @place = Place.find_by(:title => params["id"])
+      @place = Place.find_by(:id => params["id"])
       @place.delete
-      redirect_to root_url
+      redirect_to root_url # created new item in data so  item has changed.
   end
 
   def new
@@ -22,17 +22,17 @@ class PlacesController < ApplicationController
   end
 
   def edit
-      @place = Place.find_by(:title => params["id"])
+      @place = Place.find_by(:id => params["id"])
   end
 
   def update
-      @place = Place.find_by(:title => params["id"])
+      @place = Place.find_by(:id => params["id"])
       @place.title = params["title"]
       @place.price = params["price"]
       @place.description = params["description"]
       @place.photourl = params["photourl"]
       @place.save
-      redirect_to root_url
+      redirect_to "/places/#{params["id"]}"
   end
 
 end
